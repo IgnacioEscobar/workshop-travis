229b9ac8ec7e9ab8d508175449090c16dc7ad145
Ignacio Escobar <inec_14@hotmail.com>
2020-12-21 17:45:22 -0300

    Deploy test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var api = require('../routes/api.js');
 
 describe("composeGreet", function(){
     it("shall add hello to name", function(){
-        assert(api.composeGreet('Ignacio')==="hello igna\n");
+        assert(api.composeGreet('Ignacio')==="hello Ignacio\n");
     })
 })
